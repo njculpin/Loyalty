932a9906739eb7ed2c24aaa8745e94f5da3e4fb5
nick culpin <njculpin@gmail.com>
2023-05-02 13:08:47 -0700

    save

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -75,7 +75,7 @@ export default function Auth() {
         {!smartAccount && !loading && (
           <button
             onClick={Login}
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
           >
             Login
           </button>
@@ -87,7 +87,7 @@ export default function Auth() {
             <p>{smartAccount.address}</p>
             <button
               onClick={Logout}
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white mt-4"
             >
               Logout
             </button>
