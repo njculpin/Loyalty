fa82d3cd09386cee5b5931f9e958e1e5d63d80d4
nick culpin <njculpin@gmail.com>
2023-05-09 22:42:56 -0700

    store cards

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -4,6 +4,7 @@ import useStore from "../store";
 import SmartAccount from "@biconomy/smart-account";
 import { ChainId } from "@biconomy/core-types";
 import { IBalances } from "@biconomy/node-client";
+import { Network, Alchemy } from "alchemy-sdk";
 import {
   vendorCardAddress,
   patronCardAddress,
@@ -11,45 +12,72 @@ import {
 } from "../../config";
 
 export default function Mint() {
-  const [balances, setBalances] = useState<IBalances[]>([]);
-  const [gasToken, setGasToken] = useState<IBalances | null>();
-
   const smartAccount = useStore((state: any) => state.smartAccount);
+  const vendorCard = useStore((state: any) => state.vendorCard);
+  const patronCard = useStore((state: any) => state.patronCard);
+  const actions = useStore((state: any) => state.actions);
 
-  async function getBalance(smartAccount: SmartAccount) {
-    if (!smartAccount) return;
-    const balanceParams = {
-      chainId: ChainId.POLYGON_MAINNET,
-      eoaAddress: smartAccount.address,
-      tokenAddresses: [],
-    };
-    /* use getAlltokenBalances and getTotalBalanceInUsd query the smartAccount */
-    const balFromSdk = await smartAccount.getAlltokenBalances(balanceParams);
-    console.log(balFromSdk.data);
+  const settings = {
+    apiKey: process.env.ALCHEMY_TEST, // Replace with your Alchemy API Key.
+    network: Network.MATIC_MUMBAI, // Replace with your network.
+  };
 
-    setBalances(balFromSdk.data);
-    setGasToken(balFromSdk.data[0]);
+  const alchemy = new Alchemy(settings);
+
+  async function getBalance() {
+    const vendorRes = await alchemy.nft.getNftsForOwner(smartAccount.address);
+    const vendor = vendorRes.ownedNfts.find(
+      (x) => x.contract.address == vendorCardAddress.toLowerCase()
+    );
+    actions.setVendorCard(vendor);
   }
 
   useEffect(() => {
     if (smartAccount) {
-      getBalance(smartAccount);
+      getBalance();
     }
   }, []);
 
-  console.log("balances", balances);
+  console.log("vendorCard", vendorCard);
 
   return (
     <div className="space-y-12">
       <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-12 text-center shadow-2xl sm:rounded-3xl sm:px-16">
+        <div className="relative isolate overflow-hidden px-6 py-12 text-center shadow-2xl sm:rounded-3xl sm:px-16">
           <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Wallet
           </h2>
           <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
             Here are the Loyalty Assets in your Wallet:
           </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6"></div>
+          <div className="mt-10 flex items-center justify-center gap-x-6 grid-cols-2">
+            {patronCard && (
+              <div className="bg-gray-900 px-6 py-12 text-center shadow-2xl sm:rounded-3xl sm:px-16">
+                <p className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                  {patronCard.contract.name}
+                </p>
+                <p className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                  {patronCard.contract.symbol}
+                </p>
+                <p>{patronCard.contract.tokenType}</p>
+                <p>{patronCard.contract.deployedBlockNumber}</p>
+                <p>{patronCard.contract.address}</p>
+              </div>
+            )}
+            {vendorCard && (
+              <div className="bg-gray-900 px-6 py-12 text-center shadow-2xl sm:rounded-3xl sm:px-16">
+                <p className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                  {vendorCard.contract.name}
+                </p>
+                <p className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                  {vendorCard.contract.symbol}
+                </p>
+                <p>{vendorCard.contract.tokenType}</p>
+                <p>{vendorCard.contract.deployedBlockNumber}</p>
+                <p>{vendorCard.contract.address}</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/store.tsx
+++ src/store.tsx
@@ -4,9 +4,18 @@ import SmartAccount from "@biconomy/smart-account";
 const useStore = create((set, get) => {
   return {
     smartAccount: null,
+    vendorCard: null,
+    patronCard: null,
     actions: {
-      setSmartAccount: (smartAccount: SmartAccount) =>
-        set({ smartAccount: smartAccount }),
+      setSmartAccount: (smartAccount: SmartAccount) => {
+        set({ smartAccount: smartAccount });
+      },
+      setVendorCard: (vendor: any) => {
+        set({ vendorCard: vendor });
+      },
+      setPatronCard: (patron: any) => {
+        set({ patronCard: patron });
+      },
     },
   };
 });
