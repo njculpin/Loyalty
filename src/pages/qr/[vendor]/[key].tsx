7f1378bc2156331275bfd2ef1d4d69194c5fb42e
nick culpin <njculpin@gmail.com>
2023-06-05 20:13:11 -0700

    recheck points issued on submission update

--- src/pages/qr/[vendor]/[key].tsx
+++ src/pages/qr/[vendor]/[key].tsx
@@ -45,54 +45,74 @@ const Qr = () => {
   );
 
   useEffect(() => {
-    const update = async () => {
-      setLoading(true);
-      const vendorCard = await getVendorCard(vendor);
-      if (vendorCard.key != key) {
-        setLoading(false);
-        setMessage("Error! Invalid Key");
-        return;
-      } else {
-        const relationship = await getPatronCard(smartAccount.address, vendor);
-        if (relationship) {
-          setRel(relationship);
-          const update = await updateCardPoints(
-            smartAccount.address,
-            vendor,
-            key
-          );
-          if (update === "-1") {
-            setLoading(false);
-            return setMessage("Sorry, you have already checked in today!");
-          }
-        } else {
-          if (vendorCard) {
-            await createPatronCard(
-              smartAccount.address,
-              vendor,
-              vendorCard.name,
-              vendorCard.reward,
-              1,
-              vendorCard.pointCap
-            );
-          }
-        }
-        const qr = await QRCode.toDataURL(
-          `loyalty-iota.vercel.app/qr/${vendor}/${key}`
-        );
-        const newKey = uuidv4();
-        const storageRef = ref(storage, `qr/${vendor}.png`);
-        const uploadTask = await uploadString(storageRef, qr, "data_url");
-        const QRURL = uploadTask.metadata.fullPath;
-        await updateVendorCardKey(vendor, QRURL, newKey);
-        setLoading(false);
-        return;
-      }
-    };
     if (smartAccount && vendor) {
       update();
     }
   }, [vendor, key, smartAccount]);
+
+  const update = async () => {
+    setLoading(true);
+
+    const relationship = await getPatronCard(smartAccount.address, vendor);
+    setRel(relationship);
+
+    const vendorCard = await getVendorCard(vendor);
+    if (vendorCard.key != key) {
+      setLoading(false);
+      setMessage("Error! Invalid Key");
+      return;
+    } else {
+      if (relationship) {
+        setRel(relationship);
+        const update = await updateCardPoints(
+          smartAccount.address,
+          vendor,
+          key
+        );
+        if (update === "-1") {
+          setLoading(false);
+          return setMessage("Sorry, you have already checked in today!");
+        }
+      } else {
+        if (vendorCard) {
+          await createPatronCard(
+            smartAccount.address,
+            vendor,
+            vendorCard.name,
+            vendorCard.reward,
+            1,
+            vendorCard.pointCap
+          );
+        }
+      }
+
+      const result = await getPatronCard(smartAccount.address, vendor);
+      setRel(result);
+
+      issueNewKey();
+      setLoading(false);
+      return;
+    }
+  };
+
+  const issueNewKey = async () => {
+    const qr = await QRCode.toDataURL(
+      `loyalty-iota.vercel.app/qr/${vendor}/${key}`
+    );
+    const newKey = uuidv4();
+    const storageRef = ref(storage, `qr/${vendor}.png`);
+    const uploadTask = await uploadString(storageRef, qr, "data_url");
+    const QRURL = uploadTask.metadata.fullPath;
+    await updateVendorCardKey(vendor, QRURL, newKey);
+  };
+
+  const redeemCard = async () => {
+    const update = await updateCardPoints(smartAccount.address, vendor, key);
+    if (update === "-1") {
+      setLoading(false);
+      return setMessage("Sorry, you have already checked in today!");
+    }
+  };
 
   return (
     <div className="w-full">
@@ -103,16 +123,25 @@ const Qr = () => {
             <div className="grid grid-cols-1 text-center">
               <p className="text-red-500 font-bold text-lg">{message}</p>
               {rel && rel.name !== "" && !loading && (
-                <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-8xl">
-                  <span className="">You have earned </span>
-                  <span className=" text-green-400">{rel.points} </span>
-                  <span>out of </span>
-                  <span className=" text-yellow-400">{rel.pointCap} </span>
-                  <span>points for a FREE </span>
-                  <span className=" text-red-400">{rel.reward} </span>
-                  <span>from</span>
-                  <span className=" text-blue-400"> {rel.name}</span>
-                </h1>
+                <div>
+                  <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-8xl">
+                    <span className="">You have earned </span>
+                    <span className=" text-green-400">{rel.points} </span>
+                    <span>out of </span>
+                    <span className=" text-yellow-400">{rel.pointCap} </span>
+                    <span>points for a FREE </span>
+                    <span className=" text-red-400">{rel.reward} </span>
+                    <span>from</span>
+                    <span className=" text-blue-400"> {rel.name}</span>
+                  </h1>
+                  {rel.pointCap === rel.points && (
+                    <button onClick={() => redeemCard()}>
+                      <p className="border px-4 py-2 border-white">
+                        REDEEM - TEST ONLY
+                      </p>
+                    </button>
+                  )}
+                </div>
               )}
             </div>
           </div>
