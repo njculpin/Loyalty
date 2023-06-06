a75a115e14bf2a3a6952e3fce2abc8c0a0d67fb0
nick culpin <njculpin@gmail.com>
2023-06-05 19:53:01 -0700

    ignore backend during vercel build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: ["backend"],
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
