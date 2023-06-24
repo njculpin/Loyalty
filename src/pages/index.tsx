ef803917d306c7557f3bb674db29ddab2111f9ac
Nicholas Culpin <njculpin@gmail.com>
2023-06-24 11:10:12 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,36 @@ import Link from "next/link";
 import useStore from "@/lib/useStore";
 import useAuthStore from "@/lib/store";
 import { Vendor, Promotion } from "../types";
-import { CheckIcon, CloudArrowUpIcon } from "@heroicons/react/20/solid";
+import {
+  CheckIcon,
+  CloudArrowUpIcon,
+  ArrowPathIcon,
+  LockClosedIcon,
+} from "@heroicons/react/20/solid";
+
+const features = [
+  {
+    name: "Push to deploy",
+    description:
+      "Commodo nec sagittis tortor mauris sed. Turpis tortor quis scelerisque diam id accumsan nullam tempus. Pulvinar etiam lacus volutpat eu. Phasellus praesent ligula sit faucibus.",
+    href: "#",
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: "SSL certificates",
+    description:
+      "Pellentesque enim a commodo malesuada turpis eleifend risus. Facilisis donec placerat sapien consequat tempor fermentum nibh.",
+    href: "#",
+    icon: LockClosedIcon,
+  },
+  {
+    name: "Simple queues",
+    description:
+      "Pellentesque sit elit congue ante nec amet. Dolor aenean curabitur viverra suspendisse iaculis eget. Nec mollis placerat ultricies euismod ut condimentum.",
+    href: "#",
+    icon: ArrowPathIcon,
+  },
+];
 
 const includedFeatures = [
   "Food Alliance Membership",
@@ -234,43 +263,46 @@ const Index = () => {
         </div>
       </div>
       {/* MARK: USE CASE */}
-      <div className="bg-white py-24">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="ring-1 ring-gray-200 relative isolate overflow-hidden px-6 py-20 sm:rounded-3xl sm:px-10 sm:py-24 lg:py-24 xl:px-24 shadow-lg">
-            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-center lg:gap-y-0">
-              <div className="lg:row-start-2 lg:max-w-md">
-                <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">
-                  Boost your engagement.
-                  <br />
-                  Earn rewards.
-                </h2>
-                <p className="mt-6 text-lg leading-8 text-gray-900">
-                  LYLT provides a multi-directional loyalty program which
-                  rewards engagement between your business and your customers.
-                </p>
-              </div>
-              <img
-                src="/stock-scan.jpeg"
-                alt="Product screenshot"
-                className="relative -z-20 min-w-full max-w-xl rounded-xl shadow-xl ring-1 ring-white/10 lg:row-span-4 lg:w-[64rem] lg:max-w-none"
-                width={2432}
-                height={1442}
-              />
-              <div className="max-w-xl lg:row-start-3 lg:mt-10 lg:max-w-md lg:border-t lg:border-white/10 lg:pt-10">
-                <dl className="max-w-xl space-y-8 text-base leading-7 text-gray-900 lg:max-w-none">
-                  <div className="relative">
-                    <dt className="ml-9 inline-block font-semibold text-black">
-                      <CloudArrowUpIcon
-                        className="absolute left-1 top-1 h-5 w-5 text-green-500"
-                        aria-hidden="true"
-                      />
-                      Earn Points
-                    </dt>{" "}
-                    <dd className="inline">Earn</dd>
-                  </div>
-                </dl>
-              </div>
-            </div>
+      <div className="bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:text-center">
+            <h2 className="text-base font-semibold leading-7 text-green-600">
+              Deploy faster
+            </h2>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Everything you need to deploy your app
+            </p>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
+              Suspendisse eget egestas a elementum pulvinar et feugiat blandit
+              at. In mi viverra elit nunc.
+            </p>
+          </div>
+          <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+              {features.map((feature) => (
+                <div key={feature.name} className="flex flex-col">
+                  <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                    <feature.icon
+                      className="h-5 w-5 flex-none text-green-600"
+                      aria-hidden="true"
+                    />
+                    {feature.name}
+                  </dt>
+                  <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                    <p className="flex-auto">{feature.description}</p>
+                    <p className="mt-6">
+                      <a
+                        href={feature.href}
+                        className="text-sm font-semibold leading-6 text-green-600"
+                      >
+                        Learn more <span aria-hidden="true">→</span>
+                      </a>
+                    </p>
+                  </dd>
+                </div>
+              ))}
+            </dl>
           </div>
         </div>
       </div>
