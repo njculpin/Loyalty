d612c1779984d9fb48574b972b0a9279dc56ef83
Nicholas Culpin <njculpin@gmail.com>
2023-06-25 17:23:03 -0700

    save

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -59,7 +59,7 @@ export default function Nav() {
   }, [store?.wallet]);
 
   return (
-    <header className="">
+    <header className=" border-b">
       <nav
         className="flex items-center justify-between p-6 lg:px-8"
         aria-label="Global"
