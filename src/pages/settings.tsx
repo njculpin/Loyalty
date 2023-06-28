befc0dde034f0361bd1bb992587fc209923a8ada
Nicholas Culpin <njculpin@gmail.com>
2023-06-27 19:39:34 -0700

    Update settings.tsx

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -179,6 +179,7 @@ export default function Settings() {
                   name="businessCountry"
                   autoComplete="country-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  value={vendor.businessCountry}
                 >
                   <option>United States</option>
                   <option>Canada</option>
