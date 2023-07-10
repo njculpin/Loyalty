57425a51e5ba6ffceee803f480cb4ab8caa924ac
Nicholas Culpin <njculpin@gmail.com>
2023-07-09 19:44:13 -0700

    purchase modal

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -12,11 +12,13 @@ import { db } from "../firebase";
 import useStore from "@/lib/useStore";
 import useAuthStore from "@/lib/store";
 import { NFT, Wallet } from "../types";
-
-import { Transition } from "@headlessui/react";
+import { useRouter } from "next/router";
+import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon, FaceFrownIcon } from "@heroicons/react/20/solid";
+import { CheckIcon } from "@heroicons/react/24/outline";
 
 const Shop = () => {
+  const router = useRouter();
   const store = useStore(useAuthStore, (state) => state);
   const [nfts, setNFTS] = useState<NFT[]>([]);
   const [wallet, setWallet] = useState<Wallet>({
@@ -25,6 +27,7 @@ const Shop = () => {
     points: 0,
   });
   const [showError, setShowError] = useState<boolean>(false);
+  const [openModal, setOpenModal] = useState<boolean>(false);
 
   useEffect(() => {
     const queryBalance = async () => {
@@ -116,9 +119,17 @@ const Shop = () => {
         forSale: false,
         updatedAt: new Date().getTime(),
       });
+      setOpenModal(true);
+      return true;
     } catch (e) {
       console.log(e);
+      return false;
     }
+  };
+
+  const returnHome = () => {
+    setOpenModal(false);
+    return router.push(`/account`);
   };
 
   return (
@@ -159,6 +170,69 @@ const Shop = () => {
           ))}
         </div>
       </div>
+      {/* Success Purchase */}
+      <Transition.Root show={openModal} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpenModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                  <div>
+                    <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                      <CheckIcon
+                        className="h-6 w-6 text-green-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-5">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900"
+                      >
+                        Success!
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Your purchase was successful!
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-5 sm:mt-6">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+                      onClick={() => returnHome()}
+                    >
+                      Go back to dashboard
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
       <div
         aria-live="assertive"
         className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
